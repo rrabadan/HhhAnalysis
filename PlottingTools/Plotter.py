072f184a00b536ff161e5c5bb6151e420660dbcf
lpernie <luca.pernie@cern.ch>
2017-03-23 13:30:14 -0500

    Plotting

--- PlottingTools/Plotter.py
+++ PlottingTools/Plotter.py
@@ -1,13 +1,33 @@
 import random
 import ROOT
+from ROOT import TFile,TChain,TH1F,TH2F,TLegend
 import os
-execfile(start.py)
-execfile(functions.py)
+execfile("start.py")
+execfile("functions.py")
 
-filedir = "/fdata/hepx/store/user/taohuang/Hhh/combined_samples/"
-filelist = [filedir+"DiHiggs_WWbb_1M_NewB3_allReco_simulation_isomu_MVA_PU40_0824_combined.root", filedir+"DiHiggs_WWbb_1M_NewB6_allReco_simulation_isomu_MVA_PU40_0824_combined.root",filedir+"DiHiggs_WWbb_1M_NewB9_allReco_simulation_isomu_MVA_PU40_0824_combined.root","/fdata/hepx/store/user/taohuang/Hhh/Delphes_ttbar_4M_Wtomu_allReco_Updatebtag_simulation_PU40_0825/"]
-benchmarks = ["B3","B6","B9","TTbar"]
-cut = "h2tohh && htoWW && mu1_pt>10 && mu2_pt>10 && fabs(mu1_eta)<2.4 && fabs(mu2_eta)<2.4 && genmet>20"
+#Creating folders
+TT_ch = ROOT.TChain("DiHiggsWWBBAna/evtree") 
+os.system("mkdir -p Plots")
+os.system("mkdir -p HADD")
+#TChains
+os.system("find /fdata/hepx/store/user/lpernie/TTTo2L2Nu_13TeV-powheg/TTTo2L2Nu_powheg_miniAODv2_v0_ext1_01/170322_221256 | grep root > HADD/TT0.txt")
+with open('HADD/TT0.txt','r') as f:
+  for line in f:
+    if not line.isspace():
+      TT_ch.Add(str(line[:-1]))
+print "TT has", TT_ch.GetEntries(), "entries."
+filelist = [TT_ch]
+#Cuts and Ordering
+cut = "mu1_pt>10 && mu2_pt>10 && fabs(mu1_eta)<2.4 && fabs(mu2_eta)<2.4"
+benchmarks = ["TTbar","DY"]
+
+#---Starting to plot histos here-------------------------------------------------------------------------------------------------------------------------------------
+pic_name = "dR_l1l2"
+todraw   = "dR_l1l2"
+x_bins   = "(50,0.0,5)"
+x_title   = "#Delta R(l,l)"
+draw1D(filelist,todraw,x_bins,x_title,cut,benchmarks,pic_name)
+
 
 pic_name = "MMC_h2mass"
 x_title = "Most Probable mass"
@@ -97,14 +117,14 @@ todraw = "MT2_reco"
 x_bins = "(80,0.0,400)"
 x_title = "M_{T2} [GeV]"
 cut = "(h2tohh || ttbar) && hasRECOjet1 && hasRECOjet2 && hastwomuons && dR_b1jet<0.4 && dR_b2jet<0.4 && hasdRljet"
-draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
+#draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
 
 pic_name = "dR_l1l2"
 todraw = "dR_l1l2"
 x_bins = "(50,0.0,5)"
 x_title = "#Delta R(l,l)"
 cut = "(h2tohh || ttbar) && hasRECOjet1 && hasRECOjet2 && hastwomuons && dR_b1jet<0.4 && dR_b2jet<0.4 && hasdRljet"
-draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
+#draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
 pic_name = "dR_l1l2gen"
 todraw = "dR_genl1l2"
 x_title = "#Delta R(l,l), gen level"
@@ -115,7 +135,7 @@ todraw = "dR_b1b2"
 x_bins = "(50,0.0,5)"
 x_title = "#Delta R(j,j)"
 cut = "(h2tohh || ttbar) && hasRECOjet1 && hasRECOjet2 && hastwomuons && dR_b1jet<0.4 && dR_b2jet<0.4 && hasdRljet"
-draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
+#draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
 pic_name = "dR_b1b2gen"
 todraw = "dR_genb1b2"
 x_title = "#Delta R(j,j), gen level"
@@ -126,7 +146,7 @@ todraw = "dR_l1l2b1b2"
 x_bins = "(50,0.0,5)"
 x_title = "#Delta R(ll,jj)"
 cut = "(h2tohh || ttbar) && hasRECOjet1 && hasRECOjet2 && hastwomuons && dR_b1jet<0.4 && dR_b2jet<0.4 && hasdRljet"
-draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
+#draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
 pic_name = "dR_l1l2b1b2gen"
 todraw = "dR_genl1l2b1b2"
 x_title = "#Delta R(ll,jj), gen level"
@@ -137,7 +157,7 @@ todraw = "dphi_l1l2b1b2"
 x_bins = "(50,0.0,3.5)"
 x_title = "#Delta #phi(ll,jj)"
 cut = "(h2tohh || ttbar) && hasRECOjet1 && hasRECOjet2 && hastwomuons && dR_b1jet<0.4 && dR_b2jet<0.4 && hasdRljet"
-draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
+#draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
 pic_name = "dphi_l1l2b1b2gen"
 todraw = "dphi_genl1l2b1b2"
 x_title = "#Delta #phi(ll,jj), gen level"
@@ -148,7 +168,7 @@ todraw = "dphi_llmet"
 x_bins = "(50,0.0,3.5)"
 x_title = "#Delta #phi(ll, #slash{E}_{T})"
 cut = "(h2tohh || ttbar) && hasRECOjet1 && hasRECOjet2 && hastwomuons && dR_b1jet<0.4 && dR_b2jet<0.4 && hasdRljet"
-draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
+#draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
 pic_name = "dphi_llmetgen"
 todraw = "dphi_genllmet"
 x_title = "#Delta #phi(ll, #slash{E}_{T}), gen level"
@@ -159,7 +179,7 @@ todraw = "dR_minbl"
 x_bins = "(50,0.0,5)"
 x_title = "min #Delta R(l,j)"
 cut = "(h2tohh || ttbar) && hasRECOjet1 && hasRECOjet2 && hastwomuons && dR_b1jet<0.4 && dR_b2jet<0.4 && hasdRljet"#hasdRljet
-draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
+#draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
 pic_name = "dR_minblgen"
 todraw = "dR_genminbl"
 x_title = "min #Delta R(l,j), gen level"
@@ -170,7 +190,7 @@ todraw = "mass_l1l2"
 x_bins = "(50,.0,400)"
 x_title = "M(ll) [GeV]"
 cut = "(h2tohh || ttbar) && hasRECOjet1 && hasRECOjet2 && hastwomuons && dR_b1jet<0.4 && dR_b2jet<0.4 && hasdRljet"
-draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
+#draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
 pic_name = "mass_l1l2gen"
 todraw = "mass_genl1l2"
 x_title = "M(ll), gen level"
@@ -181,7 +201,7 @@ todraw = "mass_b1b2"
 x_bins = "(80,0.0,400)"
 x_title = "M(jj) [GeV]"
 cut = "(h2tohh || ttbar) && hasRECOjet1 && hasRECOjet2 && hastwomuons && dR_b1jet<0.4 && dR_b2jet<0.4 && hasdRljet"
-draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
+#draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
 pic_name = "mass_b1b2gen"
 todraw = "mass_genb1b2"
 x_title = "M(jj), gen level"
@@ -192,7 +212,7 @@ todraw = "mass_trans"
 x_bins = "(60,0.0,300)"
 x_title = "M_{trans} [GeV]"
 cut = "(h2tohh || ttbar) && hasRECOjet1 && hasRECOjet2 && hastwomuons && dR_b1jet<0.4 && dR_b2jet<0.4 && hasdRljet"
-draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
+#draw1D(filelist,"evtree",todraw,x_bins,x_title,cut,benchmarks, pic_name)
 pic_name = "mass_transgen"
 todraw = "mass_gentrans"
 x_title = "M_{trans}, gen level"
