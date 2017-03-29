e50dfecc72ffd7f2332bc15068b41106381019dc
lpernie <luca.pernie@cern.ch>
2017-03-29 17:52:32 -0500

    fix

--- CutFlowAnalyzer/test/MC_MINIAODtoNtuple/runDiHiggsWWAnalyzer.py
+++ CutFlowAnalyzer/test/MC_MINIAODtoNtuple/runDiHiggsWWAnalyzer.py
@@ -68,8 +68,7 @@ process.DiHiggsWWBBAna = cms.EDAnalyzer('DiHiggsWWBBAnalyzer',
   TrackRefitter = cms.InputTag("TrackRefitter"),
   primaryVertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
   Traj = cms.InputTag("TrackRefitter"),
-  finalStates = cms.bool(False),
-  simulation = cms.bool(True),
+  onlyGenLevel = cms.bool(False),
   debug = cms.untracked.bool(False),
   runMMC = cms.bool(False)
 )
