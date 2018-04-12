bcc3b175ff155645b05936be9629147582fedbb1
TaoHuang <taohuang@physics.tamu.edu>
2018-04-12 16:46:08 -0500

    bugfixed

--- python/DYEstimation/addDYMBtagWeightToTree.py
+++ python/DYEstimation/addDYMBtagWeightToTree.py
@@ -34,7 +34,7 @@ def get_Mbtag_weight(bdt_value,  pt1, eta1,  pt2, eta2, allfracs, Teffs):
 	    weight =+ frac*eff1*eff2
     return weight 
 
-dy_frac_eff_f = "dy_frac_eff_combined.root"
+dy_frac_eff_f = "/home/taohuang/DiHiggsAnalysis/CMSSW_9_4_0_pre1/src/HhhAnalysis/python/DYEstimation/dy_frac_eff_combined.root"
 dytf = ROOT.TFile(dy_frac_eff_f, "READ")
 allfractions = {}
 allTeffs = {}
@@ -122,7 +122,7 @@ bdt_tmva_variables = [
 #bdt_label = "2017_02_17_BDTDY_bb_cc_vs_rest_10var"
 bdt_label = "2018_04_12_BDTDY_bb_cc_vs_rest_10var"
 #FIXME
-bdt_xml_file = "/home/taohuang/DiHiggsAnalysis/CMSSW_9_4_0_pre1/src/HhhAnalysis/python/DYBDTTraining/weights/{}_kBDT.weights.xml".format(bdt_label)
+bdt_xml_file = "/home/taohuang/DiHiggsAnalysis/CMSSW_9_4_0_pre1/src/HhhAnalysis/python/DYEstimation/DYBDTTraining/weights/{}_kBDT.weights.xml".format(bdt_label)
 print "bdt_xml_file ",bdt_xml_file
 
 dict_tmva_variables = { var: array('f', [0]) for var in bdt_tmva_variables }
